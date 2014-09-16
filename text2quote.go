e81d77aa282888c26bbc66c6e3d24c0e1c901224
Michael Wendland <accounts@michiwend.com>
2014-09-17 01:20:00 +0200

    added autobackup, config file, fixes

--- text2quote.go
+++ text2quote.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"strconv"
 	"strings"
@@ -12,75 +12,111 @@ import (
 	"github.com/kennygrant/sanitize"
 )
 
+func readConfig(filename string) (map[string]string, error) {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var c map[string]string
+	json.Unmarshal(content, &c)
+
+	return c, nil
+}
+
+func writeBackup(filename string, data interface{}) error {
+
+	d, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(filename, d, 0666)
+}
+
 func main() {
 
-	blogname := flag.String("blogname", "", "the blogname like example.tumblr.com")
-	flag.Parse()
+	conf, err := readConfig("./config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	blogname := conf["blogname"]
 
 	client := gotumblr.NewTumblrRestClient(
-		"consumerKey",
-		"consumerSecret",
-		"oauthToken",
-		"oauthSecret",
-		"callbackUrl",
+		conf["consumer_key"],
+		conf["consumer_secret"],
+		conf["token"],
+		conf["token_secret"],
+		"http://localhost",
 		"http://api.tumblr.com")
 
 	response := client.Posts(
-		*blogname,
+		blogname,
 		"text",
 		map[string]string{})
 
 	fmt.Println("total text posts:", response.Total_posts)
 
-	var alltheposts []gotumblr.TextPost
-	var i int64
-	// fetch all the posts
-	for {
-		fmt.Printf("fetched posts from %d to %d\n", i, i+19)
+	if response.Total_posts > 0 {
 
-		for _, jsonpost := range response.Posts {
-			post := gotumblr.TextPost{}
-			json.Unmarshal(jsonpost, &post)
-			alltheposts = append(alltheposts, post)
+		var alltheposts []gotumblr.TextPost
+		var i int64
+		// fetch all the posts
+		for {
+			fmt.Printf("fetched posts from %d to %d\n", i, i+19)
+
+			for _, jsonpost := range response.Posts {
+				post := gotumblr.TextPost{}
+				json.Unmarshal(jsonpost, &post)
+				alltheposts = append(alltheposts, post)
+			}
+
+			if i < response.Total_posts-20 {
+				i += 20
+				response = client.Posts(
+					blogname,
+					"text",
+					map[string]string{
+						"offset": strconv.FormatInt(i, 10)})
+			} else {
+				break
+			}
 		}
 
-		if i < response.Total_posts-20 {
-			i += 20
-			response = client.Posts(
-				*blogname,
-				"text",
-				map[string]string{
-					"offset": strconv.FormatInt(i, 10)})
-		} else {
-			break
-		}
-	}
+		if alltheposts != nil {
 
-	for _, post := range alltheposts {
-
-		quote := strings.Trim(sanitize.HTML(post.Body), "\n")
-
-		if err := client.CreateQuote(
-			*blogname,
-			map[string]string{
-				"quote": quote,
-				"date":  post.Date,
-			},
-		); err == nil {
-
-			if err := client.DeletePost(
-				*blogname,
-				strconv.FormatInt(post.Id, 10),
-			); err != nil {
+			if err := writeBackup("./backup.json", alltheposts); err != nil {
 				log.Fatal(err)
 			}
 
-		} else {
-			log.Fatal(err)
+			for _, post := range alltheposts {
+
+				quote := strings.Trim(sanitize.HTML(post.Body), "\n")
+
+				if err := client.CreateQuote(
+					blogname,
+					map[string]string{
+						"quote": quote,
+						"date":  post.Date,
+					},
+				); err == nil {
+
+					if err := client.DeletePost(
+						blogname,
+						strconv.FormatInt(post.Id, 10),
+					); err != nil {
+						log.Fatal(err)
+					}
+
+				} else {
+					log.Fatal(err)
+				}
+
+				fmt.Println("created new quote:", quote)
+
+			}
 		}
-
-		fmt.Println("created new quote:", quote)
-
 	}
 
 }
